f6a82e59cf3dab89e3edd40a37238a8a90cafe70
david-gelu <david.blond@yahoo.com>
2020-06-26 19:42:15 +0300

    stilizare
    stilizare home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
+// import styled, { css } from 'styled-components';
 
 const Home = () => {
 
@@ -25,18 +26,63 @@ const Home = () => {
       });
   };
 
-  // return (
-  //   <div className="main">
-  //     <div className="pokemon">
-  //       <figure className="pokemon-container">
-  //         <a href="/pokemon"> <img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId()}.png`} alt=" pokemon" />
-  //           <figcaption>
-  //             <span className="pokemon-detail">Name: {pokemon.name}</span>
-  //           </figcaption></a>
-  //       </figure>
-  //     </div>
-  //   </div>
-  // );
+  return (
+    <div className="main">
+      <div className="pokemon">
+        <figure className="pokemon-container">
+          <Link to="/pokemon"> <img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId}.png`} alt=" pokemon" />
+            <figcaption>
+              <span className="pokemon-detail">Name: {pokemon.name}</span>
+            </figcaption></Link>
+        </figure>
+        <figure className="pokemon-container">
+          <Link to="/pokemon"> <img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId}.png`} alt=" pokemon" />
+            <figcaption>
+              <span className="pokemon-detail">Name: {pokemon.name}</span>
+            </figcaption></Link>
+        </figure>
+        <figure className="pokemon-container">
+          <Link to="/pokemon"> <img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId}.png`} alt=" pokemon" />
+            <figcaption>
+              <span className="pokemon-detail">Name: {pokemon.name}</span>
+            </figcaption></Link>
+        </figure>
+        <figure className="pokemon-container">
+          <Link to="/pokemon"> <img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId}.png`} alt=" pokemon" />
+            <figcaption>
+              <span className="pokemon-detail">Name: {pokemon.name}</span>
+            </figcaption></Link>
+        </figure>
+      </div>
+      <div className="pokemon">
+        <figure className="pokemon-container">
+          <Link to="/pokemon"> <img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId}.png`} alt=" pokemon" />
+            <figcaption>
+              <span className="pokemon-detail">Name: {pokemon.name}</span>
+            </figcaption></Link>
+        </figure>
+        <figure className="pokemon-container">
+          <Link to="/pokemon"> <img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId}.png`} alt=" pokemon" />
+            <figcaption>
+              <span className="pokemon-detail">Name: {pokemon.name}</span>
+            </figcaption></Link>
+        </figure>
+        <figure className="pokemon-container">
+          <Link to="/pokemon"> <img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId}.png`} alt=" pokemon" />
+            <figcaption>
+              <span className="pokemon-detail">Name: {pokemon.name}</span>
+            </figcaption></Link>
+        </figure>
+        <figure className="pokemon-container">
+          <Link to="/pokemon"> <img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId}.png`} alt=" pokemon" />
+            <figcaption>
+              <span className="pokemon-detail">Name: {pokemon.name}</span>
+            </figcaption></Link>
+        </figure>
+      </div>
+
+    </div>
+  );
 
   // useEffect(() => {
   //   const fetchData = async () => {
@@ -47,22 +93,22 @@ const Home = () => {
   //   fetchData();
   // }, []);
 
-  return (
-    < div className="main" >
-      {poke.posts.map(({ id, name }) => {
-        return (
-          <div className="pokemon">
-            <figure className="pokemon-container" ke>
-              <a href="/pokemon"><img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId()}.png`} alt=" pokemon" />
-                <figcaption>
-                  <span className="pokemon-detail">Name: {name}</span>
-                </figcaption></a>
-            </figure>
-          </div>
-        );
-      })}
-    </div >
-  );
+  // return (
+  //   < div className="main" >
+  //     {poke.posts.map(({ id, name }) => {
+  //       return (
+  //         <div className="pokemon">
+  //           <figure className="pokemon-container" ke>
+  //             <a href="/pokemon"><img src={`http://pokeapi.co/media/sprites/pokemon/${pokeId()}.png`} alt=" pokemon" />
+  //               <figcaption>
+  //                 <span className="pokemon-detail">Name: {name}</span>
+  //               </figcaption></a>
+  //           </figure>
+  //         </div>
+  //       );
+  //     })}
+  //   </div >
+  // );
 }
 
 export default Home;
